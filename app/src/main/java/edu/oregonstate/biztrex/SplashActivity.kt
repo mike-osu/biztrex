0040d7e68bd13880e12948e9d98f02f29dd5c997
mikeacosta <mike@acosta.net>
2021-11-05 09:24:04 -1000

    animate splash screen

--- app/src/main/java/edu/oregonstate/biztrex/SplashActivity.kt
+++ app/src/main/java/edu/oregonstate/biztrex/SplashActivity.kt
@@ -1,19 +1,57 @@
 package edu.oregonstate.biztrex
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import edu.oregonstate.biztrex.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    private var isAnimationDone = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val zoom: Animation = AnimationUtils.loadAnimation(this, R.anim.zoom)
+        binding.imageViewSplash.startAnimation(zoom)
+
+        zoom.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {
+            }
+
+            override fun onAnimationEnd(animation: Animation) {
+                isAnimationDone = true
+                Thread.sleep(2000)
+                binding.imageViewSplash.visibility = View.GONE
+                binding.splashProgressBar.visibility = View.VISIBLE
+                proceed()
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {
+            }
+        })
+    }
+
+    private fun proceed()
+    {
+        if (!isAnimationDone)
+            return
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this,MainActivity::class.java))
             finish()
-        }, 3000)
+        }, 1500L)
+
+        isAnimationDone = false
     }
 }
